7a152de0a266d15e5c9182e71825edb701556e17
ravenncrows <waitingforthen@gmail.com>
2017-01-29 22:02:27 +0700

    update V1.3 delete itemcat and testlogin

--- app/Object/Item/itemUtil/ItemCategory.php
+++ app/Object/Item/itemUtil/ItemCategory.php
@@ -6,7 +6,18 @@
  * Date: 1/29/2017
  * Time: 12:10 AM
  */
-class ItemCategory
-{
+namespace App\Object\Item\ItemUtil;
 
+use Illuminate\Database\Eloquent\Model;
+
+class ItemCategory extends Model
+{
+    public $table = 'item_category';
+
+    public $timestamps = false;
+    public $object_name = "ItemCategory";
+
+    public $columns_list = [
+        'name'=>'name'
+    ];
 }
