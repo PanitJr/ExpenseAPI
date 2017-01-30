c20f00527f5d3d80510f126918b4cef9c448fa2f
ravenncrows <waitingforthen@gmail.com>
2017-01-31 00:53:59 +0700

    update V1.3 item create and pick list

--- app/Object/Item/CCSave.php
+++ app/Object/Item/CCSave.php
@@ -11,9 +11,48 @@ class CCSave extends CSave
     {
         return true;
     }
-    
+
     public function process($request)
     {
-    	return parent::process($request);
+        $result = parent::process($request);
+        $record = (int)$request->route('record');
+        if(empty($record)){
+            if((int)$request->get('category')==1){
+                $travel = new Travel();
+                $travel->item_id = $result->id;
+                $travel->save();
+            }else if((int)$request->get('category')==2){
+                $service = new Service();
+                $service->item_id = $result->id;
+                $service->save();
+            }else if((int)$request->get('category')==3){
+                $medical = new Medical();
+                $medical->item_id = $result->id;
+                $medical->save();
+            }else if((int)$request->get('category')==4){
+                $other = new Other();
+                $other->item_id = $result->id;
+                $other->save();
+            }
+        }else{
+            if((int)$request->get('category')==1){
+                $travel = $result->travel;
+                $travel->item_id = $result->id;
+                $travel->save();
+            }else if((int)$request->get('category')==2){
+                $service = $result->service;
+                $service->item_id = $result->id;
+                $service->save();
+            }else if((int)$request->get('category')==3){
+                $medical = $result->medical ;
+                $medical->item_id = $result->id;
+                $medical->save();
+            }else if((int)$request->get('category')==4){
+                $other = $result->other;
+                $other->item_id = $result->id;
+                $other->save();
+            }
+        }
+        return $result;
     }
 }
